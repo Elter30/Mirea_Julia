e3576f81026a126ab8fa6fe34daad207fcf0bf79
Elter30 <tsvv2003@gmail.com>
2021-10-25 13:53:59 +0300

    Использование библиотеки roblib

--- Task_2/Task_2.jl
+++ Task_2/Task_2.jl
@@ -8,6 +8,8 @@ inverse(side::HorizonSide) - подключается из библиотечн�
 perimetr!(rob::Robot) - красит периметр внешней рамки маркерами и возвращает робота в исходное положение
 
 =#
+include("roblib.jl")
+
 function draw!(x::Robot, side::HorizonSide)
     while !isborder(x, side)
         move!(x, side)
@@ -15,28 +17,9 @@ function draw!(x::Robot, side::HorizonSide)
     end
 end
 
-function go_to_corner!(x::Robot, mvs::AbstractArray)
-    max_move!(x, Sud, mvs)
-    max_move!(x, West, mvs)
-end
-
-function max_move!(x::Robot, side::HorizonSide, mvs::AbstractArray)
-    while !isborder(x, side)
-        move!(x, side)
-        push!(mvs, side)
-    end
-end
-
-function return_back!(x::Robot, mvs::AbstractArray)
-    while length(mvs) > 0
-        last_move = pop!(mvs)
-        move!(x, inverse(last_move))
-    end
-end
 
 function perimeter!(rob::Robot)
     arr = []
-
     go_to_corner!(rob, arr)
 
     for s in [Nord,Ost, Sud, West]
