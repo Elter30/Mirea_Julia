27f2e53a5839dc8402585e3787d4f2dc84062ce4
Elter30 <tsvv2003@gmail.com>
2021-10-25 11:45:22 +0300

    Добавление описания задания

--- Task_1/Task_1.jl
+++ Task_1/Task_1.jl
@@ -1,22 +1,30 @@
-function draw!(x, side)
+#=
+
+ДАНО: Робот находится в произвольной клетке ограниченного прямоугольного поля без внутренних перегородок и маркеров.
+РЕЗУЛЬТАТ: Робот — в исходном положении в центре прямого креста из маркеров, расставленных вплоть до внешней рамки.
+
+krest!(rob::Robot) - рисует крест из маркеров относительно начального положения
+
+=#
+function draw!(x::Robot, side::HorizonSide)
     while !isborder(x, side)
         move!(x, side)
         putmarker!(x)
     end
 end
-function come_back!(x, side)
+function come_back!(x::Robot, side::HorizonSide)
     while ismarker(x)
         move!(x, side)
     end
 end
-function inverse(side)
+function inverse(side::HorizonSide)
     return HorizonSide((Int(side) + 2) % 4)
 end
-function krest!(rob)
+function krest!(rob::Robot)
     for s in [Nord,West, Sud, Ost]
         draw!(rob, s)
         come_back!(rob, inverse(s))
     end
-    putmarker!(r)
+    putmarker!(rob)
 end
 krest!(r)
