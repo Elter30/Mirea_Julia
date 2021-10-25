aa3924fe5b35650481b5ec8d2118f0bba247107c
Elter30 <tsvv2003@gmail.com>
2021-10-25 12:12:10 +0300

    Включение некоторых функций

--- Task_2/Task_2.jl
+++ Task_2/Task_2.jl
@@ -1,32 +1,49 @@
-function draw!(x, side)
+#=
+
+ДАНО: Робот - в произвольной клетке поля (без внутренних перегородок и маркеров)
+РЕЗУЛЬТАТ: Робот - в исходном положении, и все клетки по периметру внешней рамки промакированы
+
+inverse(side::HorizonSide) - подключается из библиотечного файла roblib.jl
+
+perimetr!(rob::Robot) - красит периметр внешней рамки маркерами и возвращает робота в исходное положение
+
+=#
+function draw!(x::Robot, side::HorizonSide)
     while !isborder(x, side)
         move!(x, side)
         putmarker!(x)
     end
 end
 
-function perimeter!(rob)
-    coun = 0
+function go_to_corner!(x::Robot, mvs::AbstractArray)
+    max_move!(x, Sud, mvs)
+    max_move!(x, West, mvs)
+end
 
-    while !isborder(rob, Ost)
-        move!(rob, Ost)
-        coun += 1
+function max_move!(x::Robot, side::HorizonSide, mvs::AbstractArray)
+    while !isborder(x, side)
+        move!(x, side)
+        push!(mvs, side)
     end
+end
 
-    for s in [Nord,West, Sud, Ost]
+function return_back!(x::Robot, mvs::AbstractArray)
+    while length(mvs) > 0
+        last_move = pop!(mvs)
+        move!(x, inverse(last_move))
+    end
+end
+
+function perimeter!(rob::Robot)
+    arr = []
+
+    go_to_corner!(rob, arr)
+
+    for s in [Nord,Ost, Sud, West]
         draw!(rob, s)
     end
-    move!(rob, Nord)
 
-    while !ismarker(rob)
-        putmarker!(rob)
-        move!(rob, Nord)
-    end
-
-    move!(rob, Sud)
-    for i in 1:coun
-        move!(rob, West)
-    end
+    return_back!(rob, arr)
 end
 
 perimeter!(r)
